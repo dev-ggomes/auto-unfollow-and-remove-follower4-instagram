fb78ca748347ff68cb6685e869ed4d5661ac6dec
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-06-26 11:33:05 +0100

    user script

---
+++ script.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+
+// @name            Instagram Quick Unfollow & Remove
+// @namespace       http://tampermonkey.net/
+// @version         1.0
+// @description     Allows you to unfollow someone by clicking "Ctrl + D" and by clicking "Ctrl + F" you can remove someone from your followers list
+// @author          dev-ggomes
+// @match           https://instagram.com/*/followers/*
+// @match           https://instagram.com/*/following/*
+// @grant           none
+// @license         MIT License
+
+// ==/UserScript==
